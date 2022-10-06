112516d299973fa2a7d2731243b48947298004c5
Cloud474 <huseyin.blt47@hotmail.com>
2022-10-06 20:45:31 +0300

    Api key vs

--- src/test/java/GoRest/GoRestUserTests.java
+++ src/test/java/GoRest/GoRestUserTests.java
@@ -1,31 +1,36 @@
 package GoRest;
-
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-
 public class GoRestUserTests {
 
     @BeforeClass
-    void Setup(){
-        baseURI="https://gorest.co.in/public/v2/";
+    void Setup() {
+        baseURI = "https://gorest.co.in/public/v2/";
     }
 
     @Test(enabled = false)
-    public void createUser(){
+    public void createUser() {
 
-        int userID=
+        int userID =
                 given()
 
-                        .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
                         .contentType(ContentType.JSON)
-                        .body("{\"name\":\""+getRandomName()+ "\",\"gender\":\"male\", \"email\":\""+getRandomEmail()+"\", \"status\":\"active\"}")
+                        .body("{\"name\":\"" + getRandomName() + "\",\"gender\":\"male\", \"email\":\"" + getRandomEmail() + "\", \"status\":\"active\"}")
 
                         .when()
                         .post("users")
@@ -38,24 +43,27 @@ public class GoRestUserTests {
 
         System.out.println("userID = " + userID);
     }
-    public String getRandomName(){
+
+    public String getRandomName() {
         return RandomStringUtils.randomAlphabetic(8);
     }
-    public String getRandomEmail(){
-        return RandomStringUtils.randomAlphabetic(8)+"@gmail.com";
-    }
-    @Test(enabled = false)
-    public void createUserMap(){
 
-        Map<String,String> newUser=new HashMap<>();
-        newUser.put("name",getRandomName());
-        newUser.put("gender","male");
-        newUser.put("email",getRandomEmail());
-        newUser.put("status","active");
-        int userID=
+    public String getRandomEmail() {
+        return RandomStringUtils.randomAlphabetic(8) + "@gmail.com";
+    }
+
+    @Test(enabled = false)
+    public void createUserMap() {
+
+        Map<String, String> newUser = new HashMap<>();
+        newUser.put("name", getRandomName());
+        newUser.put("gender", "male");
+        newUser.put("email", getRandomEmail());
+        newUser.put("status", "active");
+        int userID =
                 given()
 
-                        .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
                         .contentType(ContentType.JSON)
                         .body(newUser)
 
@@ -70,19 +78,21 @@ public class GoRestUserTests {
 
         System.out.println("userID = " + userID);
     }
-    int userID=0;
-    @Test
-    public void createUserObject(){
 
-        User newUser=new User();
+    int userID = 0;
+
+    @Test
+    public void createUserObject() {
+
+        User newUser = new User();
         newUser.setName(getRandomName());
         newUser.setGender("male");
         newUser.setEmail(getRandomEmail());
         newUser.setStatus("active");
-         userID=
+        userID =
                 given()
 
-                        .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
                         .contentType(ContentType.JSON)
                         .body(newUser)
 
@@ -97,63 +107,34 @@ public class GoRestUserTests {
 
         System.out.println("userID = " + userID);
     }
-    class User{
-       private String name;
-       private String gender;
-       private String email;
-       private String status;
 
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public String getGender() {
-            return gender;
-        }
-        public void setGender(String gender) {
-            this.gender = gender;
-        }
-        public String getEmail() {
-            return email;
-        }
-        public void setEmail(String email) {
-            this.email = email;
-        }
-        public String getStatus() {
-            return status;
-        }
-        public void setStatus(String status) {
-            this.status = status;
-        }
-    }
     @Test(dependsOnMethods = "createUserObject", priority = 1)
-    public void updateUserObject(){
+    public void updateUserObject() {
 
-        Map<String,String> updateUser=new HashMap<>();
-        updateUser.put("name","Cloud Mcgregor");
-
-                given()
-                        .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
-                        .contentType(ContentType.JSON)
-                        .body(updateUser)
-                        .log().body()
-                        .pathParam("userID", userID)
-
-                        .when()
-                        .put("users/{userID}")
-
-                        .then()
-                        .log().body()
-                        .statusCode(200)
-                        .body("name", equalTo("Cloud Mcgregor"));
-    }
-    @Test(dependsOnMethods = "createUserObject", priority = 2)
-    public void getUserById(){
+        Map<String, String> updateUser = new HashMap<>();
+        updateUser.put("name", "Cloud Mcgregor");
 
         given()
-                .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                .contentType(ContentType.JSON)
+                .body(updateUser)
+                .log().body()
+                .pathParam("userID", userID)
+
+                .when()
+                .put("users/{userID}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo("Cloud Mcgregor"));
+    }
+
+    @Test(dependsOnMethods = "createUserObject", priority = 2)
+    public void getUserById() {
+
+        given()
+                .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
                 .contentType(ContentType.JSON)
                 .log().body()
                 .pathParam("userID", userID)
@@ -166,11 +147,12 @@ public class GoRestUserTests {
                 .statusCode(200)
                 .body("id", equalTo(userID));
     }
+
     @Test(dependsOnMethods = "createUserObject", priority = 3)
-    public void deleteUserById(){
+    public void deleteUserById() {
 
         given()
-                .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
                 .contentType(ContentType.JSON)
                 .log().body()
                 .pathParam("userID", userID)
@@ -182,11 +164,12 @@ public class GoRestUserTests {
                 .log().body()
                 .statusCode(204);
     }
+
     @Test(dependsOnMethods = "deleteUserById")
-    public void deleteUserByIdNegative(){
+    public void deleteUserByIdNegative() {
 
         given()
-                .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
                 .contentType(ContentType.JSON)
                 .log().body()
                 .pathParam("userID", userID)
@@ -198,19 +181,126 @@ public class GoRestUserTests {
                 .log().body()
                 .statusCode(404);
     }
-    @Test()
-    public void getUsers(){
 
-        given()
-                .header("Authorization","Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+    @Test
+    public void getUsers() {
+        Response response =
+                given()
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
 
-                .when()
-                .get("users")
+                        .when()
+                        .get("users")
 
-                .then()
-                .log().body()
-                .statusCode(200)
-                .extract().response();
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().response();
+
+        // TODO : 3 usersın id sini alınız (path ve jsonPath ile ayrı ayrı yapınız)
+        int idUser3path = response.path("[2].id");
+        int idUser3JsonPath = response.jsonPath().getInt("[2].id");
+        System.out.println("idUser3path = " + idUser3path);
+        System.out.println("idUser3JsonPath = " + idUser3JsonPath);
+
+        User[] usersPath = response.as(User[].class);
+        System.out.println("Arrays.toString(usersPath) = " + Arrays.toString(usersPath));
+
+        List<User> usersJsonPath=response.jsonPath().getList("",User.class);
+        System.out.println("usersJsonPath = " + usersJsonPath);
     }
-}
+    @Test
+    public void getUserByIDExtract() {
+        // TODO : GetUserByID testinde dönen user ı bir nesneye atınız.
+        User user =
+                given()
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+                        .contentType(ContentType.JSON)
+                        .pathParam("userID", 3414)
 
+                        .when()
+                        .get("users/{userID}")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().jsonPath().getObject("", User.class);
+
+        System.out.println("user = " + user);
+    }
+    @Test
+    public void getUsersV1() {
+        Response response =
+                given()
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().response();
+
+        List<User> dataUsers = response.jsonPath().getList("data", User.class);
+        System.out.println("dataUsers = " + dataUsers);
+    }
+    class User {
+        private int id;
+        private String name;
+        private String gender;
+        private String email;
+        private String status;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", gender='" + gender + '\'' +
+                    ", email='" + email + '\'' +
+                    ", status='" + status + '\'' +
+                    '}';
+        }
+    }
+
+}
